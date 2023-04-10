49504a0227e1221913ddbd068be7b81eb62be89d
Yuri Zaev <juryzaev@gmail.com>
2023-04-10 22:14:46 +0700

    multilanguage support and a lot of bugs done

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import logging
 import base64
 from config import MY_TOKEN
+from dict import translations
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,12 +21,24 @@ correct_answers = {}
 scores = {}
 total_answers = {}
 options_per_chat = {}
+selected_language = {}
 
 
-def generate_trivia_button():
+async def choose_language_handler(message):
+    chat_id = message.chat.id if isinstance(message, types.Message) else message.from_user.id
+    markup = InlineKeyboardMarkup(row_width=2)
+    en_button = InlineKeyboardButton("English", callback_data="language_en")
+    ru_button = InlineKeyboardButton("Русский", callback_data="language_ru")
+    markup.add(en_button, ru_button)
+
+    await bot.send_message(chat_id, "Choose the interface language / Выберите язык интерфейса:", reply_markup=markup)
+
+
+
+def generate_trivia_button(language: str):
     markup = InlineKeyboardMarkup()
-    trivia_button = InlineKeyboardButton("Get Trivia Question", callback_data="get_trivia")
-    end_game_button = InlineKeyboardButton("End Game", callback_data="end_game")
+    trivia_button = InlineKeyboardButton(translations[language]["get_trivia_question"], callback_data="get_trivia")
+    end_game_button = InlineKeyboardButton(translations[language]["end_game"], callback_data="end_game")
     markup.add(trivia_button, end_game_button)
     return markup
 
@@ -50,14 +63,13 @@ async def format_question(data):
     return formatted_question, options, correct_answer
 
 
-async def send_response_and_buttons(chat_id, text):
-    markup = generate_trivia_button()
+async def send_response_and_buttons(chat_id, text, language):
+    markup = generate_trivia_button(language)
     await bot.send_message(chat_id, text, reply_markup=markup)
 
 
 async def start_handler(message: types.Message):
-    markup = generate_start_button()
-    await message.reply("Welcome to the Trivia Bot! Click the button to start the test.", reply_markup=markup)
+    await ask_language(message)
 
 
 async def trivia_handler(message: types.Message, difficulty: str = "easy"):
@@ -76,7 +88,8 @@ async def trivia_handler(message: types.Message, difficulty: str = "easy"):
 
     options_per_chat[chat_id] = options
 
-    await message.reply(question, parse_mode=ParseMode.HTML, reply_markup=inline_keyboard)
+    language = selected_language.get(message.chat.id, "en")
+    await bot.send_message(chat_id, question, parse_mode=ParseMode.HTML, reply_markup=inline_keyboard)
 
 
 async def end_game_handler(callback_query: types.CallbackQuery):
@@ -89,18 +102,25 @@ async def end_game_handler(callback_query: types.CallbackQuery):
         percentage = (final_score / total) * 100
     else:
         percentage = 0
+
+    # Reset the counters
+    total_answers[chat_id] = 0
+    scores[chat_id] = 0
+
     await bot.send_message(
         chat_id,
         f"Thanks for playing! Your final score is: {final_score}/{total}. "
         f"Your percentage of correct answers is: {percentage:.1f}%. Click the button to play again."
     )
+    language = selected_language.get(callback_query.message.chat.id, "en")
     markup = generate_start_button()
     await bot.send_message(chat_id, "Click the button below to start a new test.", reply_markup=markup)
 
 
+
 @dp.callback_query_handler(
     lambda c: c.data.startswith("answer_") or c.data in ["get_trivia", "end_game"] or c.data.startswith(
-        "difficulty_") or c.data == "start_test")
+        "difficulty_") or c.data == "start_test" or c.data.startswith("language_"))
 async def answer_callback_handler(callback_query: types.CallbackQuery):
     message = callback_query.message
 
@@ -116,18 +136,13 @@ async def answer_callback_handler(callback_query: types.CallbackQuery):
 
         if user_answer.lower() == correct_answer.lower():
             scores[chat_id] += 1
-            await send_response_and_buttons(
-                chat_id,
-                f"🎉 Correct! Well done! Your score is: {scores[chat_id]}/{total_answers[chat_id]}"
-            )
+            response_text = f"🎉 Correct! Well done! Your score is: {scores[chat_id]}/{total_answers[chat_id]}"
         else:
-            await send_response_and_buttons(
-                chat_id,
-                f"😢 Sorry, the correct answer was: {correct_answer}. "
-                f"Your score is: {scores[chat_id]}/{total_answers[chat_id]}"
-            )
+            response_text = f"😢 Sorry, the correct answer was: {correct_answer}." \
+                            f" Your score is: {scores[chat_id]}/{total_answers[chat_id]}"
 
-        del correct_answers[chat_id]
+        language = selected_language.get(chat_id, "en")
+        await send_response_and_buttons(chat_id, response_text, language)
 
     elif callback_query.data == "get_trivia":
         await trivia_handler(message)
@@ -137,7 +152,12 @@ async def answer_callback_handler(callback_query: types.CallbackQuery):
         difficulty = callback_query.data.split("_")[1]
         await trivia_handler(message, difficulty)
     elif callback_query.data == "start_test":
-        await start_test_handler(callback_query)
+        await choose_language_handler(callback_query)
+    elif callback_query.data.startswith("language_"):
+        language = callback_query.data.split("_")[1]
+        selected_language[callback_query.message.chat.id] = language
+        await start_test_handler(callback_query, language)
+
 
 
 def generate_start_button():
@@ -147,19 +167,32 @@ def generate_start_button():
     return markup
 
 
-async def start_test_handler(callback_query: types.CallbackQuery):
-    chat_id = callback_query.from_user.id
-    if chat_id in total_answers:
-        del total_answers[chat_id]
-        del scores[chat_id]
-
+async def start_test_handler(message, language=None):
+    chat_id = message.chat.id if isinstance(message, types.Message) else message.from_user.id
+    if language is None:
+        language = selected_language.get(chat_id, "en")
     markup = InlineKeyboardMarkup(row_width=2)
-    easy_button = InlineKeyboardButton("Easy", callback_data="difficulty_easy")
-    medium_button = InlineKeyboardButton("Medium", callback_data="difficulty_medium")
-    hard_button = InlineKeyboardButton("Hard", callback_data="difficulty_hard")
+    easy_button = InlineKeyboardButton(translations[language]["easy"], callback_data="difficulty_easy")
+    medium_button = InlineKeyboardButton(translations[language]["medium"], callback_data="difficulty_medium")
+    hard_button = InlineKeyboardButton(translations[language]["hard"], callback_data="difficulty_hard")
     markup.add(easy_button, medium_button, hard_button)
 
-    await bot.send_message(chat_id, "Выберите уровень сложности:", reply_markup=markup)
+    await bot.send_message(chat_id, translations[language]["choose_difficulty"], reply_markup=markup)
+
+
+def generate_language_buttons():
+    markup = InlineKeyboardMarkup(row_width=2)
+    en_button = InlineKeyboardButton("English", callback_data="language_en")
+    ru_button = InlineKeyboardButton("Русский", callback_data="language_ru")
+    markup.add(en_button, ru_button)
+    return markup
+
+
+async def ask_language(message: types.Message):
+    markup = generate_language_buttons()
+    await message.reply("Choose the interface language / Выберите язык интерфейса:", reply_markup=markup)
+
+
 
 
 dp.register_message_handler(start_handler, commands=["start", "help"])
---
+++ dict.py
@@ -0,0 +1,20 @@
+translations = {
+    "en": {
+        "welcome": "Welcome to the Trivia Bot! Click the button to start the test.",
+        "choose_difficulty": "Choose the difficulty level:",
+        "easy": "Easy",
+        "medium": "Medium",
+        "hard": "Hard",
+        "get_trivia_question": "Get Trivia Question",
+        "end_game": "End Game",
+    },
+    "ru": {
+        "welcome": "Добро пожаловать в Trivia Bot! Нажмите кнопку, чтобы начать тест.",
+        "choose_difficulty": "Выберите уровень сложности:",
+        "easy": "Легко",
+        "medium": "Средне",
+        "hard": "Тяжело",
+        "get_trivia_question": "Получить вопрос",
+        "end_game": "Завершить игру",
+    },
+}
